6a4d502fb9d634b10cf3ee8b6cc3aede5fdb5278
Matias <matiasmicheletto@gmail.com>
2020-03-21 18:28:51 -0300

    Formulario datos usuario

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,14 +2,15 @@ app.controller("home", ['$scope', '$rootScope', function ($scope, $rootScope) {
 
     $rootScope.showPreloader("Determinando ubicación...");
     middleware.checkLocation() // Obtener ubicacion del usuario
-    .then(function(position) { // Usuario dentro del rango
+    .then(function() { // Usuario dentro del rango
         $rootScope.hidePreloader();
-        $rootScope.userAllowed = true;
+        $rootScope.$apply();
     })
     .catch(function(err){ // Usuario fuera del rango
         console.log(err);
         $rootScope.hidePreloader();
         $rootScope.userAllowed = false;
+        $rootScope.$apply();
     });
     
 }]);
